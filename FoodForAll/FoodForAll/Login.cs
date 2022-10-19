031e28b76cb75f4e03802fc7ba975f5bdc76b401
wowmom98 <rma6182@rit.edu>
2022-10-18 22:39:48 -0400

    Create visual studio project, create visuals of the first form using Winforms
    First form is the Login form. The visuals are done, however actual functionality is yet to be implemented.

---
+++ FoodForAll/FoodForAll/Login.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FoodForAll
+{
+	public partial class Login : Form
+	{
+		public Login()
+		{
+			InitializeComponent();
+		}
+
+		private void RegisterLink_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+		{
+			//TODO
+		}
+
+		private void SignInButton_Click(object sender, EventArgs e)
+		{
+			//TODO
+		}
+	}
+}
